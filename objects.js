ab156a9bdf5839c45c0f1bebbb2447d533671adb
RachitaBas <rachitarachita123@gmail.com>
2021-11-14 04:58:34 +0545

    inheritance commit

--- objects.js
+++ objects.js
@@ -1,22 +1,40 @@
-
-function User(email,name){
-    this.email=email;
-    this.online=false;
-    this.online=false;
-    
+class User {
+    constructor(email,name){
+this.email= email;
+this.name = name;
+this.score=0;//at the beginning
     }
-
-User.prototype.login=function(){   //using prototype for loggin in 
-this.online=true;
-console.log(this.email,'has logged in');
+    login(){       //methods function
+        console.log(this.email,'just logged in');
+        return this; //for method chaining
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this; //for method chaining
+    }
+    updateScore(){
+        this.score++;//increasing score
+        console.log(this.email,'score is now',this.score);
+        return this; //for method chaining
+    }
 }
-User.prototype.logout=function(){     ///for logging out
-    this.online=false;
-    console.log(this.email,'has logged out');
-
+class Admin extends User { //Inheritance
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email ;
+        })
+    }
 }
+var userOne= new User('rachita1@gmail.com','Rach');
+var userTwo= new User('raju21@gmail.com','Raju');
+var admin = new Admin ('rabina2@gmail.com','rabina');
+var users = [userOne,userTwo,admin];
+//admin.deleteUser(userOne);
+console.log(users);
+// userOne.login();//class methods
+// userTwo.logout();//class methods
+userOne.login().updateScore().updateScore().logout();//method chaining will run now one by one
 
-    var userOne = new User('rach1@gmail.com','Rach');
-var userTwo = new User('samir2@gmail.com','samir');
-console.log(userOne);
-userTwo.login();
+// console.log(userOne);
+// console.log(userTwo);
+
