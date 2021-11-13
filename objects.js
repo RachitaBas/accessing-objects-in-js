95c16374ca88d589563cac328a18f399784b2bb0
RachitaBas <rachitarachita123@gmail.com>
2021-11-13 17:16:04 +0545

    object commit

--- objects.js
+++ objects.js
@@ -1,28 +1,11 @@
-class User {
-     constructor(email,name){
-this.email= email;
-this.name = name;
-this.score=0;//at the beginning
-     }
-     login(){       //methods function
-         console.log(this.email,'just logged in');
-         return this; //for method chaining
-     }
-     logout(){
-         console.log(this.email, 'just logged out');
-         return this; //for method chaining
-     }
-     updateScore(){
-         this.score++;//increasing score
-         console.log(this.email,'score is now',this.score);
-         return this; //for method chaining
-     }
-}
-var userOne= new User('rachita1@gmail.com','Rach');
-var userTwo= new User('raju21@gmail.com','Raju');
-// userOne.login();//class methods
-// userTwo.logout();//class methods
-userOne.login().updateScore().updateScore().logout();//method chaining will run now one by one
+var userOne= {
+    email:'rachita123@gmail.com',
+    name:'rachita',
+    login(){
+        console.log(this.email,'has logged in');
 
-// console.log(userOne);
-// console.log(userTwo);
+    },
+    logout(){
+        console.log(this.email,'has logged out');
+    }
+};
