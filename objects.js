d470d2ca945cd5136e92260078063d3ea36d3e69
RachitaBas <rachitarachita123@gmail.com>
2021-11-14 04:52:23 +0545

    prototyping commit

--- objects.js
+++ objects.js
@@ -2,12 +2,21 @@
 function User(email,name){
     this.email=email;
     this.online=false;
-    this.login=function(){
-        console.log(this.email,'has logged in');
-
+    this.online=false;
+    
     }
+
+User.prototype.login=function(){   //using prototype for loggin in 
+this.online=true;
+console.log(this.email,'has logged in');
 }
-var userOne = new User('rach1@gmail.com','Rach');
+User.prototype.logout=function(){     ///for logging out
+    this.online=false;
+    console.log(this.email,'has logged out');
+
+}
+
+    var userOne = new User('rach1@gmail.com','Rach');
 var userTwo = new User('samir2@gmail.com','samir');
 console.log(userOne);
 userTwo.login();
